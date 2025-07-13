840d4ccd520011631d300ce4c56f496bef2eb2f2
yinon115 <yinon115@hotmail.com>
2025-07-13 14:40:29 +0300

    Setup publishing for JitPack

--- jsontreeview/build.gradle.kts
+++ jsontreeview/build.gradle.kts
@@ -1,21 +1,20 @@
 plugins {
     alias(libs.plugins.android.library)
+    id("maven-publish")
 }
 
 android {
-
-    buildFeatures {
-        dataBinding = true
-    }
-
     namespace = "com.example.jsontreeview"
     compileSdk = 35
 
     defaultConfig {
         minSdk = 24
-
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildFeatures {
+        dataBinding = true
     }
 
     buildTypes {
@@ -27,17 +26,36 @@ android {
             )
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+
+    publishing {
+        singleVariant("release") {
+            withSourcesJar()
+        }
+    }
 }
 
 dependencies {
-
     implementation(libs.appcompat)
     implementation(libs.material)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+}
+
+afterEvaluate {
+    extensions.configure<org.gradle.api.publish.PublishingExtension>("publishing") {
+        publications {
+            create<MavenPublication>("release") {
+                from(components["release"])
+                groupId = "com.github.yinon42"
+                artifactId = "jsontreeview"
+                version = "1.0.0"
+            }
+        }
+    }
 }
